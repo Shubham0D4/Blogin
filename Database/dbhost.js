08d3f998d521e3e01229fa3d8723ea9526fe5d5a
ShubhamDarekar <129769325+Shubham0D4@users.noreply.github.com>
2024-09-12 22:15:17 +0530

    Update dbhost.js

--- Database/dbhost.js
+++ Database/dbhost.js
@@ -3,8 +3,8 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host:'localhost',
-    user:"root",
-    password:"2005",
+    user:"yourusernamehere",
+    password:"####",
     database:"Blogin"
 });
 connection.connect((err)=>{
